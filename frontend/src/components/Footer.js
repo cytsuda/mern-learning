e30b52c02a5242bf4380f9459aa6abfc99430ec2
Carlos Yoshio Tsuda <cyt231@gmail.com>
2020-11-04 16:20:13 -0300

    React-Bootstrap Setup, Header & Footer Components

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+const Footer = () => {
+  return (
+    <footer className="mt-auto">
+      <Container>
+        <p className="text-center">
+          Copyright &copy; NoobShop - 2020
+        </p>
+      </Container>
+    </footer>
+  );
+};
+
+export default Footer;
