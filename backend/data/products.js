f52a2be8e9ef8f278034082017ed062be065aeb1
Carlos Yoshio Tsuda <cyt231@gmail.com>
2020-11-05 17:03:30 -0300

    Serving Products - Back End Routes

--- backend/data/products.js
+++ backend/data/products.js
@@ -15,7 +15,7 @@ const products = [
   {
     _id: "2",
     name: "iPhone 11 Pro 256GB Memory",
-    image: "/images/Sphone.jpg",
+    image: "/images/phone.jpg",
     description:
       "Introducing the iPhone 11 Pro. A transformative triple-camera system that adds tons of capability without complexity. An unprecedented leap in battery life",
     brand: "Apple",
