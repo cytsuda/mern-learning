182425049879cb67dabddfb19c93ea02e949a7b4
Carlos Yoshio Tsuda <cyt231@gmail.com>
2020-11-09 09:46:54 -0300

    Create a Redux Store

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
+import store from "./store";
 import App from "./App";
 import "./bootstrap.min.css";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <Router>
       <App />
     </Router>
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById("root")
 );
 
