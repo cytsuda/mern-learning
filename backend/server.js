e798054fa5913381d90581c0e23614070d6a7dd2
Carlos Yoshio Tsuda <cyt231@gmail.com>
2020-11-10 16:53:57 -0300

    C7L2 - User Authentication Endpoint2

--- backend/server.js
+++ backend/server.js
@@ -4,19 +4,22 @@ import connectDB from "./config/db.js";
 // Routes
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+
+const PORT = process.env.PORT || 5000;
+const NODE_ENV = process.env.NODE_ENV;
 
 dotenv.config();
-
 connectDB();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
-const NODE_ENV = process.env.NODE_ENV;
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("API is runing...");
 });
 app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
