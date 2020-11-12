146470e3d11e03fa45273c9ebc2776add9ec6f2c
Carlos Yoshio Tsuda <cyt231@gmail.com>
2020-11-12 16:17:41 -0300

    C9L1 - Shipping Screen & Save Address

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -21,6 +21,10 @@ const userInfoFromStorage = localStorage.getItem("userInfo")
   ? JSON.parse(localStorage.getItem("userInfo"))
   : null;
 
+const shippingAddressFromStorage = localStorage.getItem("shippingAddress")
+  ? JSON.parse(localStorage.getItem("shippingAddress"))
+  : {};
+
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
@@ -32,7 +36,7 @@ const reducer = combineReducers({
 });
 
 const initialState = {
-  cart: { cartItems: cartItemsFromStorage },
+  cart: { cartItems: cartItemsFromStorage, shippingAddress: shippingAddressFromStorage },
   userLogin: { userInfo: userInfoFromStorage },
 };
 
