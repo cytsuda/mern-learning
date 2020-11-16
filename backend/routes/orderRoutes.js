96d80c85b2f09ba6073bbc38b1e9e4d57ffe5298
Carlos Yoshio Tsuda <cyt231@gmail.com>
2020-11-16 15:48:33 -0300

    C10L4 - Update To Paid Endpoint

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,9 +1,14 @@
 import express from "express";
 const router = express.Router();
-import { addItemsOrder, getOrderById } from "../controller/orderController.js";
+import {
+  addItemsOrder,
+  getOrderById,
+  updateOrderToPaid,
+} from "../controller/orderController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 router.route("/").post(protect, addItemsOrder);
 router.route("/:id").get(protect, getOrderById);
+router.route("/:id/pay").put(protect, updateOrderToPaid);
 
 export default router;
